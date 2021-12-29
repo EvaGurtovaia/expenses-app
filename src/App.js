01bf140b8e442440d924b84ab6e1f509b2b17d44
EvaGurtovaia <eva.gurtovaya@gmail.com>
2021-12-29 17:06:04 -0600

    Splitting components into multiple components

--- src/App.js
+++ src/App.js
@@ -7,25 +7,25 @@ function App() {
             id: "e1",
             title: "Toilet Paper",
             amount: 94.12,
-            date: "November 13",
+            date: new Date(2020, 7, 14),
         },
         {
             id: "e2",
             title: "New TV",
             amount: 799.49,
-            date: "December 11",
+            date: new Date(2021, 2, 12),
         },
         {
             id: "e3",
             title: "Car Insurance",
             amount: 294.67,
-            date: "December 21",
+            date: new Date(2021, 2, 28),
         },
         {
             id: "e4",
             title: "New Desk (Wooden)",
             amount: 450,
-            date: "December 29",
+            date: new Date(2021, 5, 12),
         },
     ];
     return (
