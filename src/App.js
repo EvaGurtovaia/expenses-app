fddb6e33c0c1269ee2713a830ae1280f4ca057be
EvaGurtovaia <eva.gurtovaya@gmail.com>
2021-12-29 16:20:29 -0600

    Working on ExpenseItem component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const expenses = [
+        {
+            id: "e1",
+            title: "Toilet Paper",
+            amount: 94.12,
+            date: "November 13",
+        },
+        {
+            id: "e2",
+            title: "New TV",
+            amount: 799.49,
+            date: "December 11",
+        },
+        {
+            id: "e3",
+            title: "Car Insurance",
+            amount: 294.67,
+            date: "December 21",
+        },
+        {
+            id: "e4",
+            title: "New Desk (Wooden)",
+            amount: 450,
+            date: "December 29",
+        },
+    ];
+    return (
+        <div className="App">
+            <ExpenseItem
+                key={expenses[0].id}
+                title={expenses[0].title}
+                amount={expenses[0].amount}
+                date={expenses[0].date}
+            />
+            <ExpenseItem
+                key={expenses[1].id}
+                title={expenses[1].title}
+                amount={expenses[1].amount}
+                date={expenses[1].date}
+            />
+            <ExpenseItem
+                key={expenses[2].id}
+                title={expenses[2].title}
+                amount={expenses[2].amount}
+                date={expenses[2].date}
+            />
+            <ExpenseItem
+                key={expenses[3].id}
+                title={expenses[3].title}
+                amount={expenses[3].amount}
+                date={expenses[3].date}
+            />
+        </div>
+    );
 }
 
 export default App;
