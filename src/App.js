04e40140d6b2817410125a3690942a2c46e5b220
EvaGurtovaia <eva.gurtovaya@gmail.com>
2021-12-30 17:54:42 -0600

    Listening to user input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Expenses from "./components/Expenses";
+import NewExpense from "./components/NewExpense";
 
 function App() {
     const expenses = [
@@ -30,6 +31,7 @@ function App() {
     ];
     return (
         <div className="App">
+            <NewExpense />
             <Expenses expenses={expenses} />
         </div>
     );
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,8 +1,31 @@
-const ExpenseForm = () =>{
-    return <div>
-        <form></form>
-    </div>
-};
+import "./ExpenseForm.css";
 
+const ExpenseForm = () => {
+    const titleChangeHandler = ()=> {
+
+    }
+    return (
+        <form>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label >Title</label>
+                    <input type="text"  onChange={titleChangeHandler}/>
+                </div>
+                <div className="new-expense__control">
+                    <label>Amount</label>
+                    <input type="number" min="0.01" step="0.01" onChange={} />
+                </div>
+
+                <div className="new-expense__control">
+                    <label>Date</label>
+                    <input type="date" min="2021-01-01" onChange={}/>
+                </div>
+            </div>
+            <div className="new-expense__actions">
+                <button type="submit">Add Expense</button>
+            </div>
+        </form>
+    );
+};
 
 export default ExpenseForm;
