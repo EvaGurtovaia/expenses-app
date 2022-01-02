bc77936209ad726bea39d0a42526a1285d09357c
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-01-02 16:38:32 -0600

    Working on ExpensesFilter component

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,35 +1,40 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "./Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
     return (
-        <Card className="expenses">
-            <ExpenseItem
-                key={props.expenses[0].id}
-                title={props.expenses[0].title}
-                amount={props.expenses[0].amount}
-                date={props.expenses[0].date}
-            />
-            <ExpenseItem
-                key={props.expenses[1].id}
-                title={props.expenses[1].title}
-                amount={props.expenses[1].amount}
-                date={props.expenses[1].date}
-            />
-            <ExpenseItem
-                key={props.expenses[2].id}
-                title={props.expenses[2].title}
-                amount={props.expenses[2].amount}
-                date={props.expenses[2].date}
-            />
-            <ExpenseItem
-                key={props.expenses[3].id}
-                title={props.expenses[3].title}
-                amount={props.expenses[3].amount}
-                date={props.expenses[3].date}
-            />
-        </Card>
+        <div>
+           
+            <Card className="expenses">
+            <ExpensesFilter />
+                <ExpenseItem
+                    key={props.expenses[0].id}
+                    title={props.expenses[0].title}
+                    amount={props.expenses[0].amount}
+                    date={props.expenses[0].date}
+                />
+                <ExpenseItem
+                    key={props.expenses[1].id}
+                    title={props.expenses[1].title}
+                    amount={props.expenses[1].amount}
+                    date={props.expenses[1].date}
+                />
+                <ExpenseItem
+                    key={props.expenses[2].id}
+                    title={props.expenses[2].title}
+                    amount={props.expenses[2].amount}
+                    date={props.expenses[2].date}
+                />
+                <ExpenseItem
+                    key={props.expenses[3].id}
+                    title={props.expenses[3].title}
+                    amount={props.expenses[3].amount}
+                    date={props.expenses[3].date}
+                />
+            </Card>
+        </div>
     );
 }
 
